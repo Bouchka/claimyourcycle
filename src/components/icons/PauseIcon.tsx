8a1cc681f46b4f0936c32aa815800df79c271f19
Bouchra Kostet <bouchra.kostet@gmail.com>
2024-12-22 13:07:05 +0530

    some ui changes

--- src/components/icons/PauseIcon.tsx
+++ src/components/icons/PauseIcon.tsx
@@ -2,16 +2,14 @@ export function PauseIcon({ className }: { className?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
       viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
+      fill="currentColor"
       className={className}
     >
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15.75 5.25v13.5m-7.5-13.5v13.5"
+        fillRule="evenodd"
+        d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z"
+        clipRule="evenodd"
       />
     </svg>
   );
--- src/components/icons/RightDoubleArrow.tsx
+++ src/components/icons/RightDoubleArrow.tsx
@@ -2,10 +2,8 @@ export function RightDoubleArrow({ className }: { className?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
       viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
+      fill="none"
       className={className}
     >
       <defs>
@@ -15,10 +13,8 @@ export function RightDoubleArrow({ className }: { className?: string }) {
         </linearGradient>
       </defs>
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill="url(#gradient)" /* Ensures gradient fill */
-        d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+        fill="url(#gradient)"
+        d="M5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v8.122c0 1.44 1.555 2.343 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256l-7.108-4.061C13.555 6.346 12 7.249 12 8.689v2.34L5.055 7.061Z"
       />
     </svg>
   );
